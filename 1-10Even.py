2f5e2f4eef1c125489bd97d954a2ace7fcb05d6d
Larry Chiem <33429605+KFChinese@users.noreply.github.com>
2020-01-26 18:11:06 -0800

    Create 1-10Even.py

---
+++ 1-10Even.py
@@ -0,0 +1,12 @@
+# Printing from 1-10 inclusively with even numbers
+# Source https://youtu.be/fBOgxIxv2ic?t=490 
+for x in range(10):
+    if x == 10:
+        
+        break
+
+    else:
+      if (x%2 ==1):
+        x+=1
+        print(x)
+    
